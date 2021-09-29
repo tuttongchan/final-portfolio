12611ea57397d65a3b8722fa9b6d27edb161e6d1
Tut Tongchantra <tuttongchan@Tuts-MacBook-Air.local>
2021-09-29 00:03:00 -0700

    projects section

--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -2,9 +2,74 @@ import React from 'react';
 import './projects.css';
 
 const Projects = () => {
-  return <div className="projects-container">
-      <h4>03. Some Things I've Built</h4>
-  </div>;
+  return (
+    <div className="projects-container">
+      <div className="projects-heading-container">
+        <h4 className="projects-heading">03. Some Things I've Built</h4>
+        <div className="line"></div>
+      </div>
+      <div className="projects-content-container">
+        <div className="projects-picture"></div>
+        <div className="projects-desc">
+          <h6>Featured Project</h6>
+          <h2>Travel Map</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+            voluptatem quae sit ut aperiam. Officiis vel error officia culpa
+            tenetur et minima deleniti minus sapiente, esse placeat sequi saepe
+            excepturi.
+          </p>
+          <ul>
+            <li>React</li>
+            <li>Express</li>
+            <li>MongoDB</li>
+            <li>Shopify API</li>
+            <li>Context API</li>
+          </ul>
+        </div>
+      </div>
+      <div className="projects-content-container">
+        <div className="projects-picture"></div>
+        <div className="projects-desc">
+          <h6>Featured Project</h6>
+          <h2>Travel Map</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+            voluptatem quae sit ut aperiam. Officiis vel error officia culpa
+            tenetur et minima deleniti minus sapiente, esse placeat sequi saepe
+            excepturi.
+          </p>
+          <ul>
+            <li>React</li>
+            <li>Express</li>
+            <li>MongoDB</li>
+            <li>Shopify API</li>
+            <li>Context API</li>
+          </ul>
+        </div>
+      </div>
+      <div className="projects-content-container">
+        <div className="projects-picture"></div>
+        <div className="projects-desc">
+          <h6>Featured Project</h6>
+          <h2>Travel Map</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+            voluptatem quae sit ut aperiam. Officiis vel error officia culpa
+            tenetur et minima deleniti minus sapiente, esse placeat sequi saepe
+            excepturi.
+          </p>
+          <ul>
+            <li>React</li>
+            <li>Express</li>
+            <li>MongoDB</li>
+            <li>Shopify API</li>
+            <li>Context API</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
